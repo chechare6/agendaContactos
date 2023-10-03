511a9f36200b809a1afc7d45a91814bc06f90736
Cesar Garcia <cesargarciaviana@gmail.com>
2023-10-03 20:40:19 +0200

    Todos los metodos menos borrar contacto

--- AgendaContacto/src/main/java/agendaContactos.java
+++ AgendaContacto/src/main/java/agendaContactos.java
@@ -23,19 +23,29 @@ public class agendaContactos {
 			System.out.println("-------------------------");
 			switch (option) {
 			case 1:
-				if(existeAgenda(ruta)) {
+				if (existeAgenda(ruta)) {
 					System.out.print("Escribe la UUID del contacto: ");
 					String id = sc.nextLine();
-					System.out.println(buscarCod(ruta, id));				
+					System.out.println(buscarCod(ruta, id));
 				} else {
 					System.out.println("No existen datos en la agenda.");
 				}
 				break;
 			case 2:
-				buscarNombre(null);
+				if (existeAgenda(ruta)) {
+					System.out.print("Escribe el nombre del contacto (o sus primeras letras): ");
+					String nombre = sc.nextLine();
+					System.out.println(buscarNombre(ruta, nombre));
+				} else {
+					System.out.println("No existen datos en la agenda.");
+				}
 				break;
 			case 3:
-				mostrarLista();
+				if (existeAgenda(ruta)) {
+					mostrarAgenda(ruta);
+				} else {
+					System.out.println("No existen datos en la agenda");
+				}
 				break;
 			case 4:
 				System.out.print("Nombre del nuevo contacto: ");
@@ -48,7 +58,13 @@ public class agendaContactos {
 				addContacto(nombre, tlf, edad);
 				break;
 			case 5:
-				borrarContacto(null);
+				if (existeAgenda(ruta)) {
+					System.out.print("Escribe la UUID del contacto a eliminar: ");
+					String id = sc.nextLine();
+					System.out.println(borrarContacto(ruta, id));
+				} else {
+					System.out.println("No existen datos en la agenda");
+				}
 				break;
 			case 6:
 				System.out.println("Agenda cerrada");
@@ -67,26 +83,45 @@ public class agendaContactos {
 		File agenda = new File(ruta);
 		return agenda.exists();
 	}
-	
+
 	public static String buscarCod(String ruta, String id) {
 		try {
 			BufferedReader br = new BufferedReader(new FileReader(ruta));
 			String datos;
-			while((datos = br.readLine()) != null) {
+			br.readLine(); // Lee la cabecera para no volver a leerla después
+			while ((datos = br.readLine()) != null) {
 				String[] contactos = datos.split(";");
-				if(contactos[0] == id) {					
-					return new Contacto(UUID.fromString(contactos[0]), contactos[1], contactos[2], Integer.parseInt(contactos[3])).toString();
+				for (int i = 0; i < contactos.length; i++) {
+					if (contactos[i].equals(id)) {
+						return contactos[i] + " | " + contactos[i + 1] + " | " + contactos[i + 2] + " | "
+								+ contactos[i + 3];
+					}
 				}
 			}
 		} catch (Exception e) {
 			e.getMessage();
 		}
-		return null;
+		return "No se ha encontrado al usuario con UUID: " + id;
 	}
 
-	public static Contacto buscarNombre(String nombre) {
-		System.out.println("Opción buscar por nombre");
-		return null;
+	public static String buscarNombre(String ruta, String nombre) {
+		try {
+			BufferedReader br = new BufferedReader(new FileReader(ruta));
+			String datos;
+			br.readLine(); // Lee la cabecera para no volver a leerla después
+			while ((datos = br.readLine()) != null) {
+				String[] contactos = datos.split(";");
+				for (int i = 1; i < contactos.length; i++) {
+					if (contactos[i].contains(nombre)) {
+						return contactos[i - 1] + " | " + contactos[i] + " | " + contactos[i + 1] + " | "
+								+ contactos[i + 2];
+					}
+				}
+			}
+		} catch (Exception e) {
+			e.getMessage();
+		}
+		return "No se ha encontrado al usuario con nombre (o que contenga): " + nombre;
 	}
 
 	public static void addContacto(String nombre, String telefono, int edad) {
@@ -95,12 +130,13 @@ public class agendaContactos {
 			try {
 				if (raf.length() > 0) {
 					raf.seek(raf.length());
-					raf.writeChars("\n");
+					raf.writeBytes("\n");
 				} else {
-					raf.writeChars("UUID;Nombre;Teléfono;Edad\n");
+					raf.writeBytes("UUID;Nombre;Telefono;Edad\n");
 				}
 				Contacto nCont = new Contacto(nombre, telefono, edad);
-				raf.writeChars(String.format("%s;%s;%s;%s", nCont.getIdContacto(), nCont.getNombre(), nCont.getTelefono(), nCont.getEdad()));
+				raf.writeBytes(String.format("%s;%s;%s;%s", nCont.getIdContacto(), nCont.getNombre(),
+						nCont.getTelefono(), nCont.getEdad()));
 				raf.close();
 				System.out.println("-------------------------");
 				System.out.println("Creado con éxito");
@@ -112,12 +148,44 @@ public class agendaContactos {
 		}
 	}
 
-	public static void borrarContacto(UUID id) {
-		System.out.println("Opción borrar contacto");
+	//CORREGIR ESTE METODO
+	public static String borrarContacto(String ruta, String id) {
+		String idBorrada = "borrado -> 00000000-0000-0000-0000-000000000000";
+		try {
+			RandomAccessFile raf = new RandomAccessFile(ruta, "rw");
+			String datos = raf.readLine();
+			while ((datos = raf.readLine()) != null) {
+                String[] contactos = datos.split(";");
+                String idABorrar = contactos[0].trim();
+                if (idABorrar.equals(id)) {
+                    String nuevaLinea = idBorrada + ";" + contactos[1] + ";" + contactos[2] + ";" + contactos[3];
+                    raf.writeBytes(nuevaLinea);
+                    return "Contacto con UUID: " + id + " borrado.";
+                }
+            }
+			return "UUID no encontrada";
+		} catch (Exception e) {
+			e.getMessage();
+		}
+		return "No se ha encontrado un contacto con UUID: " + id;
 	}
 
-	public static void mostrarLista() {
-		System.out.println("Opción mostrar lista");
+	public static void mostrarAgenda(String ruta) {
+		try {
+			BufferedReader bfr = new BufferedReader(new FileReader(ruta));
+			String linea;
+			while ((linea = bfr.readLine()) != null) {
+				String[] columnas = linea.split(";");
+				for (String columna : columnas) {
+					System.out.print(columna + " | ");
+				}
+				System.out.println();
+
+			}
+		} catch (IOException e) {
+			System.err.println("ERROR");
+		}
+
 	}
 
-}
+};
